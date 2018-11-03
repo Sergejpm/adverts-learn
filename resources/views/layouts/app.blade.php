fdf6d05c49db8b9442571802eb2042c4f9f1bcd8
Сергей Пушкарев <steeplol123@gmail.com>
2018-11-03 14:57:05 +0300

    Installed davejamesmiller/laravel-breadcrumbs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,6 +76,8 @@
 
     <main class="app-content py-3">
         <div class="container">
+            @section('breadcrumbs', Breadcrumbs::render())
+            @yield('breadcrumbs')
             @include('layouts.partials.flash')
             @yield('content')
         </div>
