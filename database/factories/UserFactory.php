dffc3a9eb290510441743bc7bd7d5259823023e7
Сергей Пушкарев <steeplol123@gmail.com>
2018-11-10 17:12:10 +0300

    Added user phone

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
 /*
@@ -16,10 +18,15 @@ use Faker\Generator as Faker;
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 $factory->define(App\Entity\User::class, function (Faker $faker) {
+    $phoneActive = $faker->boolean;
     return [
         'name' => $faker->name,
         'last_name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->unique()->phoneNumber,
+        'phone_verified' => $phoneActive,
+        'phone_verify_token' => $phoneActive ? null : Str::uuid(),
+        'phone_verify_token_expire' => $phoneActive ? null : Carbon::now()->addSeconds(300),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
         'email_verified_at' => now(),
