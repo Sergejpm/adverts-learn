57427684564b31d08532af1f60f20da78cadc375
Сергей Пушкарев <steeplol123@gmail.com>
2018-11-18 19:32:16 +0300

    Changed factories

--- database/factories/RegionFactory.php
+++ database/factories/RegionFactory.php
@@ -7,7 +7,6 @@ use Faker\Generator as Faker;
 $factory->define(\App\Entity\Region::class, function (Faker $faker) {
     return [
         'name' => $faker->unique()->city,
-        'slug' => $faker->unique()->slug(2),
         'parent_id' => null,
     ];
 });
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,7 @@ $factory->define(App\Entity\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'last_name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
+        'phone_auth' => 0,
         'phone' => $faker->unique()->phoneNumber,
         'phone_verified' => $phoneActive,
         'phone_verify_token' => $phoneActive ? null : Str::uuid(),
@@ -30,5 +31,6 @@ $factory->define(App\Entity\User::class, function (Faker $faker) {
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
         'email_verified_at' => now(),
+        'role' => 'user'
     ];
 });
